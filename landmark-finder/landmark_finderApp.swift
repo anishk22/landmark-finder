f4215aa2635aa39390f183850eeb2af73293e3de
Anish Kothapalli <anishvk@umich.edu>
2023-05-15 14:31:10 -0400

    Initial Commit

---
+++ landmark-finder/landmark_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  landmark_finderApp.swift
+//  landmark-finder
+//
+//  Created by Anish Kothapalli on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct landmark_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
