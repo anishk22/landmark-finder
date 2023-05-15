c75a6db72af37a8d55aeded0db35ba9410af3acf
Anish Kothapalli <anishvk@umich.edu>
2023-05-15 19:57:50 -0400

    added pins + cleaned up transition code

--- landmark-finder/Views/LocationsView.swift
+++ landmark-finder/Views/LocationsView.swift
@@ -14,38 +14,15 @@ struct LocationsView: View {
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $vm.mapRegion)
+            mapLayer
                 .ignoresSafeArea()
             
             VStack(spacing: 0) {
-                
                 header
-                .padding()
+                    .padding()
                 
                 Spacer()
-                 
-                ZStack {
-                    ForEach(vm.locations) { location in
-                        if vm.mapLocation == location {
-                            if vm.locationMovedForward {
-                                LocationPreviewView(location: location)
-                                    .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                    .padding()
-                                    .transition(.asymmetric(
-                                        insertion: .move(edge: .trailing),
-                                        removal: .move(edge: .leading)))
-                            }
-                            else {
-                                LocationPreviewView(location: location)
-                                    .shadow(color: Color.black.opacity(0.3), radius: 20)
-                                    .padding()
-                                    .transition(.asymmetric(
-                                        insertion: .move(edge: .leading),
-                                        removal: .move(edge: .trailing)))
-                            }
-                        }
-                    }
-                }
+                locationsPreviewStack
             }
         }
     }
@@ -90,5 +67,36 @@ extension LocationsView {
         .background(.thickMaterial)
         .cornerRadius(10)
         .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 15)
+    }
+    
+    private var mapLayer: some View {
+        Map(coordinateRegion: $vm.mapRegion,
+            annotationItems: vm.locations,
+            annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinates) {
+                LocationMapAnnotationView()
+                    .scaleEffect(vm.mapLocation == location ? 1 : 0.7)
+                    .shadow(radius: 10)
+                    .onTapGesture {
+                        vm.showNextLocation(location: location)
+                    }
+            }
+        })
+    }
+    
+    private var locationsPreviewStack: some View {
+        ZStack {
+            ForEach(vm.locations) { location in
+                if vm.mapLocation == location {
+                    LocationPreviewView(location: location)
+                        .shadow(color: Color.black.opacity(0.3), radius: 20)
+                        .padding()
+                        .transition(.asymmetric(
+                            insertion: vm.locationMovedForward ? .move(edge: .trailing) : .move(edge: .leading),
+                            removal: vm.locationMovedForward ? .move(edge: .leading) : .move(edge: .trailing)
+                        ))
+                }
+            }
+        }
     }
 }
