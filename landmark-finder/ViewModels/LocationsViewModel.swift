a7c51b738d822ac12c791a5213e03ad2eb0efa9f
Anish Kothapalli <anishvk@umich.edu>
2023-05-15 18:50:24 -0400

    added preview popup with next functionality

--- landmark-finder/ViewModels/LocationsViewModel.swift
+++ landmark-finder/ViewModels/LocationsViewModel.swift
@@ -56,4 +56,28 @@ class LocationsViewModel: ObservableObject {
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed() {
+        // Get the current index
+        guard let currentIndex = locations.firstIndex(where: { $0 == mapLocation }) else {
+            print("Could not find current index in locations array")
+            return
+        }
+        
+        // Check if next index is valid
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            // Next index is not valid
+            guard let firstLocation = locations.first else {
+                print("No first element in locations array")
+                return
+            }
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // Next index is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+    }
 }
